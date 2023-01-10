2427c1602bd5dbfca37f9b0711afa6f2982a00f6
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-01-10 13:00:48 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HandlingEvents from './component/HandlingEvents/HandlingEvents'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HandlingEvents />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/HandlingEvents/HandlingEvents.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './HandlingEvents.css';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            defaultStr: 'Введите текст...',
+            buttonStr: 'Отправить'
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input'),
+              res = document.querySelector('.res');
+        if (!input.value) {
+            this.setState({
+                defaultStr: 'Вы не ввели текст...',
+                buttonStr: 'Сообщение не отправлено'
+            });
+            return false;
+        } else {
+            this.setState({
+                defaultStr: 'Вы ввели текст...',
+                buttonStr: 'Сообщение отправлено',
+            });
+            res.innerHTML += input.value + '<br>';
+        };
+        input.value = '';
+        setTimeout(() => {
+            this.setState({
+                defaultStr: 'Введите текст...',
+                buttonStr: 'Отправить'
+            });
+        }, 3000);
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder={this.state.defaultStr} />
+                    <button>{this.state.buttonStr}</button>
+                </form>
+                <p className="res"></p>
+            </div>
+
+        )
+    }
+}
+
+export default Form;
